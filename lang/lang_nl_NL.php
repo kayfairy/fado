8163490c55ddc896851572822e42f185d7ef3bcf
Ferda Kamakov <fadowebap@gmail.com>
2025-09-12 21:16:54 +0200

    dutch

---
+++ lang/lang_nl_NL.php
@@ -0,0 +1,526 @@
+<?php
+/*
+
+* Taalbestand voor Fado
+
+*
+
+* nl_NL.UTF8
+
+*
+
+*
+
+*
+
+*
+
+*
+
+*/
+
+
+
+$lang = array(
+
+ 'PROGRAM_NAME' => 'Fado',
+
+ 'PROGRAM_TITLE' => 'Fado',
+
+ 'PROGRAM_TITLE_LOGIN' => 'Fado - Inloggen',
+
+ 'NAV_SHOP' => 'Winkels',
+
+ 'NAV_ROUTES' => 'Nieuwe route',
+
+ 'NAV_VEHICLES' => 'Voertuigen',
+
+ 'NAV_INVOICE' => 'Facturen',
+
+ 'NAV_MENU_SETTINGS' => 'Instellingen',
+
+ 'NAV_MENU_LOGOUT' => 'Uitloggen',
+
+ 'NAV_WAREHOUSE' => 'Magazijn',
+
+ 'NAV_INVOICES' => 'Facturen',
+
+ 'NAV_NEW_INVOICE' => 'Nieuwe factuur',
+
+ 'NAV_MEETINGS' => 'Vergaderingen',
+
+ 'SHOPS_LABEL_DESCRIPTION' => 'Winkelnaam',
+
+ 'SHOP_ADD' => 'Nieuwe winkel toevoegen',
+
+ 'SHOP_ADDED' => 'Nieuwe winkel toegevoegd',
+
+ 'SHOPS_LABEL_STREET' => 'Straatnaam',
+
+ 'SHOPS_LABEL_STREETNR' => 'Straatnr.',
+
+ 'SHOPS_LABEL_ZIP' => 'Postcode',
+
+ 'SHOPS_LABEL_CITY' => 'Stad',
+
+ 'SHOPS_LABEL_COUNTRY' => 'Land',
+
+ 'SHOPS_LABEL_PHONE' => 'Telefoon',
+
+ 'SHOPS_LABEL_EMAIL' => 'E-mail',
+
+ 'SHOP_COORDS_SAVED' => 'Nieuwe coördinaten opgeslagen',
+
+ 'SHOPS_LABEL_LOCATION' => 'Locatie',
+
+ 'SHOPS_LABEL_CREATED' => 'Aangemaakt',
+
+ 'SHOPS_LABEL_CHANGED' => 'Laatst gewijzigd',
+
+ 'SHOPS_LABEL_OPEN' => 'Opent',
+
+ 'SHOPS_LABEL_CLOSE' => 'Sluit',
+
+ 'SHOPS_LABEL_ESTABLISHED' => 'Opgericht',
+
+ 'SHOPS_LABEL_WEEKDAYPERIOD' => 'Openingstijden',
+
+ 'SHOPS_LABEL_OPENING_HOURS' => 'Openingstijden',
+
+ 'SHOP_DELETED' => 'Winkel succesvol verwijderd',
+
+ 'SHOPS_LABEL_INFORMATION' => 'Informatie',
+
+ 'SHOP_DELETE_ERROR' => 'Fout bij het verwijderen van de winkel',
+
+ 'SHOP_SAVED' => 'Winkel succesvol opgeslagen',
+
+ 'SHOP_ADDED' => 'Nieuwe winkel succesvol toegevoegd',
+
+ 'SHOP_SAVE_ERROR' => 'Fout bij het opslaan van de winkel',
+
+ 'SHOP_ADD_ERROR' => 'Fout bij toevoegen winkel',
+
+ 'SHOP_PANEL_HEADING' => 'Winkelpaneel',
+
+ 'SHOP_COORDS_SAVE' => 'Opgeslagen',
+
+ 'SHOP_PANEL_MAP' => 'Locatie',
+
+ 'SHOP_PANEL_DATA' => 'Winkelgegevens',
+
+ 'SHOP_PANEL_VEHICLE' => 'Voertuigen',
+
+ 'SHOP_PANEL_DATA_COORDS_LNG' => 'Lengtegraad',
+
+ 'SHOP_PANEL_DATA_COORDS_LAT' => 'Breedtegraad',
+
+ 'SHOP_PANEL_ADDRESS' => 'Adres',
+
+ 'SHOP_PANEL_USER' => 'Gebruikers',
+
+ 'SHOPS_TABLE_HEADER' => 'Winkellijst',
+
+ 'TABLE_NEXT' => 'Volgende',
+
+ 'TABLE_PREVIOUS' => 'Vorige',
+
+ 'BUTTON_LOGIN' => 'Inloggen',
+
+ 'LOGIN_TEXT' => 'Meld u aan',
+
+ 'LOGIN_USERNAME' => 'Gebruikersnaam',
+
+ 'LOGIN_PASSWORD' => 'Wachtwoord',
+
+ 'LOGIN_REMEMBER_ME' => 'Onthoud mij',
+
+ 'INVALID_AUTH' => 'Onjuiste naam of wachtwoord',
+
+ 'INVALID_AUTH_TOO_MANY_ATTEMPTS' => 'Meer dan %s keer geprobeerd in te loggen',
+
+ 'USER_LABEL_NAME' => 'Naam',
+
+ 'USER_LABEL_PWD' => 'Wachtwoord',
+
+ 'USER_LABEL_ACTIVE' => 'Actief',
+
+ 'USER_LABEL_ONLINE' => 'Online',
+
+ 'USER_LABEL_DATA' => 'Gegevens',
+
+ 'USER_LABEL_PHONE' => 'Telefoon',
+
+ 'USER_LABEL_EMAIL' => 'E-mail',
+
+ 'USER_LABEL_CREATED' => 'Aangemaakt',
+
+ 'USER_LABEL_CHANGED' => 'Gewijzigd',
+
+ 'USER_LABEL_LAST_LOGIN' => 'Laatste login',
+
+ 'USER_LABEL_BIRTHDAY' => 'Verjaardag',
+
+ 'USER_DATA_PANEL' => 'Gebruiker',
+
+ 'USER_DATA_SHOP' => 'Winkel',
+
+ 'USER_2_SHOP_LABEL' => 'Toegewezen winkels',
+
+ 'USER_TABLE_HEADER' => 'Gebruikerspaneel',
+
+ 'USER_ADD_ERROR' => 'Gebruikersnaam is al in gebruik.',
+
+ 'USER_ADDED' => 'Succes',
+
+ 'USER_SAVED' => 'Geslaagd',
+
+ 'USER_SAVE_ERROR' => 'Fout',
+
+ 'USER_DELETE_ERROR' => 'Fout',
+
+ 'USER_DELETED' => 'Succes',
+
+ 'USER_TO_SHOP_ADDED' => 'Geslaagd',
+
+ 'USER_TO_SHOP_ERROR' => 'Fout',
+
+ 'USER_PANEL_HEADING' => 'Gebruikersinformatie',
+
+ 'USER_DATA_RIGHTS' => 'Gebruikersrechten',
+
+ 'USER_RIGHTS_PANEL' => 'Rechtentabel',
+
+ 'USER_RIGHTS_LABEL_PAGE' => 'Gebied',
+
+ 'USER_RIGHTS_LABEL_RULE' => 'Regel',
+
+ 'USER_RIGHTS_SAVED' => 'Nieuwe rechten opgeslagen',
+
+ 'USER_RIGHTS_SAVE_ERROR' => 'Fout bij het opslaan van rechten',
+
+ 'VEHICLES_TABLE_HEADER' => 'Voertuigen',
+
+ 'VEHICLES_LABEL_CONSTRUCTION_YEAR' => 'Bouwjaar',
+
+ 'VEHICLES_LABEL_MANUFACTURER' => 'Fabrikant',
+
+ 'VEHICLES_LABEL_NAME' => 'Voertuig',
+
+ 'VEHICLES_LABEL_TYPE' => 'Type',
+
+ 'VEHICLES_LABEL_IN_USE' => 'Vrij',
+
+ 'VEHICLES_LABEL_KEYNR' => 'Sleutelnummer',
+
+ 'VEHICLES_LABEL_ACTIVE' => 'Actief',
+
+ 'VEHICLES_LABEL_CREATED' => 'Datum toegevoegd',
+
+ 'VEHICLES_LABEL_CHANGED' => 'Datum gewijzigd',
+
+ 'VEHICLE_PANEL_DATA' => 'Voertuig',
+
+ 'VEHICLE_TO_SHOP_ADDED' => 'Geslaagd',
+
+ 'VEHICLE_TO_SHOP_ADD_ERROR' => 'Fout',
+
+ 'VEHICLE_SAVED' => 'Succes',
+
+ 'VEHICLE_ADDED' => 'Succes',
+
+ 'VEHICLE_PANEL_SHOPS' => 'Winkel',
+
+ 'VEHICLE_2_SHOP_LABEL' => 'Winkel',
+
+ 'VEHICLE_PANEL_HEADING' => 'Voertuigdetails',
+
+ 'VEHICLE_DELETED' => 'Voertuig verwijderd',
+
+ 'VEHICLE_DELETE_ERROR' => 'Fout',
+
+ 'VEHICLE_SAVE_ERROR' => 'Fout',
+
+ 'VEHICLE_SAVED' => 'Voertuig opgeslagen',
+
+ 'VEHICLE_ADD_ERROR' => 'Fout bij toevoegen van voertuig',
+
+ 'VEHICLE_ADDED' => 'Nieuw voertuig toegevoegd',
+
+ 'WAREHOUSE_ITEMS_TABLE_HEADER' => 'Grondstoffen',
+
+ 'WAREHOUSE_PANEL_HEADING' => 'Artikelgegevens',
+
+ 'WAREHOUSE_PANEL_DATA' => 'Artikelgegevens',
+
+ 'WAREHOUSE_ITEMS_LABEL_NAME' => 'Artikelnaam',
+
+ 'WAREHOUSE_ITEMS_LABEL_SHOP_ID' => 'Magazijn',
+
+ 'WAREHOUSE_ITEMS_LABEL_POS' => 'Positie',
+
+ 'WAREHOUSE_ITEMS_LABEL_INFORMATION' => 'Beschrijving',
+
+ 'WAREHOUSE_ITEMS_LABEL_AMOUNT' => 'Bedrag',
+
+ 'WAREHOUSE_ITEMS_LABEL_UNIT' => 'Eenheid',
+
+ 'WAREHOUSE_ITEMS_LABEL_PRICE_UNIT' => 'Prijs [%s]',
+
+ 'WAREHOUSE_ITEMS_LABEL_CURRENCY' => 'Valuta',
+
+ 'WAREHOUSE_ITEMS_LABEL_ACTIVE' => 'Actief',
+
+ 'WAREHOUSE_ITEMS_LABEL_SOLD' => 'Verkocht',
+
+ 'WAREHOUSE_2_SHOP_LABEL' => 'Magazijn',
+
+ 'WAREHOUSE_ITEMS_LABEL_TAX_RATE' => 'Belasting [%%]',
+
+ 'WAREHOUSE_ITEMS_LABEL_CUT_RATE' => 'Snijpercentage [%%]',
+
+ 'WAREHOUSE_ITEM_LABEL_IS_PAYED' => 'Betaald',
+
+ 'ITEM_DELETED' => 'Item verwijderd',
+
+ 'ITEM_DELETE_ERROR' => 'Fout',
+
+ 'ITEM_SAVE_ERROR' => 'Fout bij opslaan',
+
+ 'ITEM_SAVED' => 'Item opgeslagen',
+
+ 'ITEM_ADD_ERROR' => 'Fout bij toevoegen van item',
+
+ 'ITEM_ADDED' => 'Nieuw item toegevoegd',
+
+ 'INVOICE_ADDED' => 'Opgeslagen',
+
+ 'INVOICES_TABLE_HEADER' => 'Facturen',
+
+ 'INVOICES_LABEL_CUSTOMER' => 'Klant',
+
+ 'INVOICES_LABEL_QUANTITY' => 'Hoeveelheid',
+
+ 'INVOICES_LABEL_SELLER' => 'Verkoper',
+
+ 'INVOICES_LABEL_ID' => 'Factuurnummer',
+
+ 'INVOICES_LABEL_SHOP_ID_SELLER' => 'Verkoper',
+
+ 'INVOICES_LABEL_SHOP_ID_CUSTOMER' => 'Klant',
+
+ 'INVOICES_LABEL_IS_PAYED' => 'Betaald',
+
+ 'INVOICES_LABEL_CREATED' => 'Aangemaakt',
+
+ 'INVOICES_LABEL_CHANGED' => 'Laatst gewijzigd',
+
+ 'INVOICES_LABEL_SUM_NETTO' => 'Netto totaal',
+
+ 'INVOICES_LABEL_SUM_BRUTTO' => 'Totaal',
+
+ 'INVOICES_LABEL_TAX' => 'BTW',
+
+ 'INVOICE_SAVED' => 'Succesvol',
+
+ 'INVOICE_DELETE_ERROR' => 'Fout',
+
+ 'INVOICE_SAVE_ERROR' => 'Fout',
+
+ 'INVOICE_DELETE_ERROR' => 'Fout',
+
+ 'INVOICES_LABEL_VAT_BRUTTO' => 'incl. VAT',
+
+ 'INVOICES_LABEL_VAT_NETTO' => 'BTW',
+
+ 'INVOICES_TITLE' => 'Factuur',
+
+ 'INVOICE_MULTIPLIER' => '×',
+
+ 'INVOICE_PER_ITEM' => 'aan',
+
+ 'INVOICES_LABEL_DATE' => 'Datum',
+
+ 'INVOICES_LABEL_PRICE' => 'Prijs',
+
+ 'INVOICE_MAIL_SUCCESS' => 'E-mail succesvol verzonden',
+
+ 'MEETINGS_TABLE_HEADER' => 'Vergadering',
+
+ 'MEETINGS_LABEL_TITLE' => 'Titel',
+
+ 'MEETINGS_LABEL_DESCRIPTION' => 'Beschrijving',
+
+ 'MEETINGS_LABEL_MEETING' => 'Tijd',
+
+ 'MEETINGS_LABEL_CREATED' => 'Aangemaakt',
+
+ 'MEETINGS_LABEL_DELETE' => 'Verwijderen',
+
+ 'MEETING_PANEL_HEADING' => 'Nieuwe vergadering',
+
+ 'MEETING_UNIT_MINUTE' => 'min',
+
+ 'MEETING_SAVE_SUCCESS' => 'Vergadering opgeslagen',
+
+ 'MEETING_SAVE_ERROR' => 'Fout bij het opslaan van de vergadering',
+
+ 'MEETING_DELETED' => 'Vergadering verwijderd',
+
+ 'MEETING_DELETE_ERROR' => 'Vergadering niet verwijderd',
+
+ 'SETTINGS_ADMIN_EDIT' => 'Serverinstellingen',
+
+ 'SETTINGS_ADMIN_OPEN_EDIT' => 'Serverinstellingen openen',
+
+ 'SETTINGS_USER_EDIT' => 'Gebruikersinstellingen',
+
+ 'SETTINGS_USER_OPEN_EDIT' => 'Gebruikersinstellingen openen',
+
+ 'SETTINGS_MAP_EDIT' => 'Kaartserverinstellingen',
+
+ 'SETTINGS_MAP_OPEN_EDIT' => 'Open kaartserverinstellingen',
+
+ 'SETTINGS_DATABASE' => 'Database',
+
+ 'SETTINGS_OPEN_SQL_IMPORT' => 'Import/Export openen',
+
+ 'SETTINGS_EDIT' => 'Instellingen',
+
+ 'SETTINGS_OPEN_EDIT' => 'Instellingen openen',
+
+ 'SETTINGS_MONEY_FORMAT' => 'Geld',
+
+ 'SETTINGS_CURRENCY_LABEL' => 'Valuta',
+
+ 'SETTINGS_NUMBER_FORMAT' => 'Formaat [decimalen, decimale punt, duizendtallen scheidingsteken]',
+
+ 'SETTINGS_LANG' => 'Taal',
+
+ 'SETTINGS_DATE_TIMEZONE' => 'Tijdzone',
+
+ 'SETTINGS_DATETIMEFORMAT_LABEL' => 'Datum/tijd-indeling',
+
+ 'SETTINGS_DATEFORMAT_LABEL' => 'Datumnotatie',
+
+ 'SETTINGS_VAT_STATUS' => 'Belasting over de toegevoegde waarde (btw) actief',
+
+ 'SETTINGS_INVOICE_MODE' => 'Factuurbelastingmodus',
+
+ 'SETTINGS_TAX_RATES_PERCENT' => 'Beschikbare belastingtarieven [%]',
+
+ 'SETTINGS_LIST_COUNT' => 'Minimaal aantal lijstitems',
+
+ 'SETTINGS_AMOUNT_UNITS' => 'Beschikbare eenheden',
+
+ 'SETTINGS_COOKIE' => 'Cookiebeheer',
+
+ 'SETTINGS_COOKIE_ID' => 'Cookienaam',
+
+ 'SETTINGS_COOKIE_LIFE_HOURS' => 'Cookielevensduur [u]',
+
+ 'SETTINGS_MEMCACHE_ON' => 'Memcache AAN',
+
+ 'SETTINGS_MEMCACHE_HOST' => 'Hostadres',
+
+ 'SETTINGS_MEMCACHE_PORT' => 'Hostpoort',
+
+ 'SETTINGS_MEMCACHE_LIFE' => 'Variabele levensduur [s]',
+
+ 'SETTINGS_MAP_SERVER' => 'Kaartinstellingen',
+
+ 'SETTINGS_MAP_TILE_URL' => 'Tegelserver-service-URL',
+
+ 'SETTINGS_MAP_ROUTE_URL' => 'URL van routeringsserverservice',
+
+ 'SETTINGS_DISPLAY_ERRORS' => 'Weergavefouten',
+
+ 'SETTINGS_SAVED' => 'Nieuwe instellingen succesvol opgeslagen',
+
+ 'SETTINGS_SAVE_ERROR' => 'Fout',
+
+ 'SERVER_INFO' => 'Webserverinfo',
+
+ 'SERVER_INFO_OPEN' => 'Open informatiesite',
+
+ 'SETTINGS_REWRITE_ON' => 'URL-herschrijfengine ingeschakeld',
+
+ 'SAVE' => 'Opslaan',
+
+ 'DELETE' => 'Verwijderen',
+
+ 'RESET' => 'Resetten',
+
+ 'CANCEL' => 'Annuleren',
+
+ 'FORWARD' => 'Vooruit',
+
+ 'HOURS' => 'uren',
+
+ 'SECONDS' => 'seconden',
+
+ 'MODAL_DELETE_HEADLINE' => 'Verwijderen',
+
+ 'MODAL_DELETE_TEXT' => 'Bevestig de actie.',
+
+ 'ERROR_FIELD_INVALID' => 'Ongeldig',
+
+ 'ERROR_FIELD_EMPTY' => 'Lege waarde',
+
+ 'ERROR_FIELD_EMAIL_INVALID' => 'Ongeldig e-mailadres',
+
+ 'ROUTE_PICKER_PANEL_LABEL' => 'Routes kiezen',
+
+ 'ROUTE_PICKER_TEXT' => 'Route kiezen',
+
+ 'ROUTES_PICKER_SHOPS' => 'Winkellijst',
+
+ 'ROUTES_PICKER_VEHICLES' => 'Voertuigen',
+
+ 'ROUTES_PICKER_INVOICES' => 'Facturen',
+
+ 'ROUTES_PICKER_WAREHOUSE_ITEMS' => 'Artikelen',
+
+ 'SHOW_ENTRIES_TEXT' => 'toon %d items',
+
+ 'TABLE_ENTRIES' => 'items %d tot %d van %d',
+
+ 'SQL_PANEL' => 'Database',
+
+ 'RUN_SQL' => 'SQL-bestand uitvoeren',
+
+ 'DOWNLOAD_SQL' => 'Database downloaden',
+
+ 'SQL_RUN_SUCCESS' => 'Nieuwe database succesvol geïmporteerd',
+
+ 'SQL_RUN_ERROR' => 'Fout tijdens importeren',
+
+ 'SQL_FILE_ERROR' => 'Geen bestand',
+
+ 'PHP_INFO_HEADING' => 'PHP-informatie',
+
+ 'PHP_MEMCACHE_STATUS' => 'Memcache-status',
+
+ 'PHP_MEMCACHE_KEY' => 'Variabele',
+
+ 'PHP_MEMCACHE_VALUE' => 'Waarde',
+
+ 'EMPTY_CACHE' => 'Cache leegmaken',
+
+ 'EMPTY_CACHE_SUCCESS' => 'Cache legen succesvol',
+
+ 'ERROR_404' => 'Fout 404',
+
+ 'ERROR_404_TEXT' => 'Pagina niet gevonden.',
+
+ 'ERROR_403' => 'Fout 403',
+
+ 'ERROR_403_TEXT' => 'Toestemming geweigerd.',
+
+/***/
+
+);
+
+
+
+return $lang;

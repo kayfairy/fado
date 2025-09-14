4cbe5bc0ba17ab559bcad27dfa7e5994b455956f
Ferda Kamakov <fadowebapp@gmail.com>
2025-09-14 04:25:49 -0400

    icu77

--- docker/compilephp.sh
+++ docker/compilephp.sh
@@ -14,13 +14,13 @@ if [ "$pak" = true ]; then
    dpkg-statoverride --remove "/etc/ssl/private"
    dpkg-statoverride --remove "/usr/lib/dbus-1.0/dbus-daemon-launch-helper"
    apt update && apt upgrade -y
-   apt install -y build-essential autoconf libtool bison re2c git wget wget2 unzip tar patch libc6-dev
+|   apt install -y build-essential autoconf libtool bison re2c git wget wget2 unzip tar patch libc6-dev
 fi
 
 if [ "$down" = true ]; then
    wget2 -O zlib.tar.gz https://www.zlib.net/zlib-1.3.1.tar.gz
    wget2 -O oniguruma.zip https://github.com/kkos/oniguruma/archive/refs/tags/v6.9.10.zip
-   wget2 -O icu.zip https://github.com/unicode-org/icu/releases/download/release-72-1/icu4c-72_1-src.zip
+   wget2 -O icu.zip https://github.com/unicode-org/icu/releases/download/release-77-1/icu4c-77_1-src.tgz
    wget2 -O libxml.tar.xz https://download.gnome.org/sources/libxml2/2.14/libxml2-2.14.0.tar.xz
    wget2 -O sqlite.zip https://sqlite.org/2025/sqlite-src-3500200.zip
    wget2 -O openssl.tar.gz https://github.com/openssl/openssl/releases/download/openssl-3.5.1/openssl-3.5.1.tar.gz

8900642c9f08ed80b5259418f1f272a04894fda7
CEO Ferda <admin@fado.com>
2025-12-15 18:21:53 +0100

    line count

--- wc.sh
+++ wc.sh
@@ -1,43 +1,39 @@
 #!/bin/bash
 wcll () {
-    if [ $2 == 0 ]; then
+    if [ $2 = 0 ]; then
         cat "$1/c.txt"
         cdir="$1"
-    elif [true]; then
+    elif [ true ]; then
         cdir="$3"
         d="$2"
     fi
-    cat "$cdir/c.txt"
-    i=$(( $? ))
+    i=$( cat "$cdir/c.txt" | xargs expr )
     y=""
     z=""
-    for s in $(ls -x $1)
+    for s in $( ls -x $1 )
     do
-        stat "$1/$s"
-        y=$(echo "$?" | grep File)
-        stat "$1/$s"
-        z=$(echo "$?" | grep directory)
+        y=$( stat $s | grep file )
+        z=$( stat $s | grep directory )
         if [ "$z" != ""  ]; then
-           echo "Enter $1/$s"
            cd "$1/$s/"
            d=$(( $d + 1 ))
-           wcll "$1/$s" "$d" "$cdir"
-           i=$(( $i + $? ))
+           zz=$( wcll "$1/$s" "$d" "$cdir" )
+           i=$(( $i + $zz ))
            cd "$1"
         elif [ "$y" != "" ]; then
-            wc -l "$1/$s" | grep -o -E '(^[0-9]+?)[[:space:]].+?$'
-            if [ "$?" != "" ]; then
-                wc -l "$1/$s" | grep -o -E '(^[0-9]+?)[[:space:]].+?$'
-                i=$(( $i + $? ))
+            x=$( wc -l "$1/$s" | grep -o -E "[0-9]+[[:space:]]" | grep -o -E "[0-9]+" )
+            if [ "$x" != "" ]; then
+                i=$(( $i + $x ))
             fi
         fi
     done
+    rm "$cdir/c.txt"
     echo "$i" > "$cdir/c.txt"
-    return "$i";
+    echo "$i"
+    return 0;
 }
 echo 0 > "$PWD/c.txt"
 wcll $PWD 0 $PWD
-echo $?
 echo "line count is "
 cat $PWD/c.txt
 exit 0

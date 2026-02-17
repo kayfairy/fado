fefd13ded20e8e21391624a8d8547ddca6c3a3c5
Ferda K. <fadowebapp@gmail.com>
2026-02-17 17:29:27 +0100

    * < * *

--- docker/fish_sh_deploy.sh
+++ docker/fish_sh_deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #udocker run --entrypoint="/bin/bash -f /var/www/html/docker/fish_sh_deploy.sh" --volume="/data/data/com.termux/files/home/github/fado/:/var/www/html/" debian:forky
-bin=$(which fish)
-is=$( which fish  | wc -l | wc -l )
-if [ $is -eq "1" ]; then
+is=$( which fish )
+if [[ $is != "" ]]; then
     /bin/fish
     exit 0
 fi
